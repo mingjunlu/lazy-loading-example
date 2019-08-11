3cd34010b534d92207e2bf1616d2e9f93b825442
mingjun <40516784+mingjunlu@users.noreply.github.com>
2019-08-11 13:30:51 +0800

    Edit some comments

--- scripts/noscriptFallback.js
+++ scripts/noscriptFallback.js
@@ -1,3 +1,4 @@
+// 如果瀏覽器有開啟 JavaScript 的話，就移除 document 的 'no-js' class
 const { documentElement } = document
 documentElement.classList.length > 1
     ? documentElement.classList.remove('no-js')
--- scripts/supportCheck.js
+++ scripts/supportCheck.js
@@ -1,4 +1,4 @@
-// Use the polyfill if Intersection Observer is not supported
+// 如果瀏覽器不支援 Intersection Observer 的話就引入 polyfill
 if (!window.IntersectionObserver) {
     const polyfillUrl = 'https://polyfill.io/v3/polyfill.min.js?features=IntersectionObserver'
     const polyfillScript = document.createElement('script')
